5fae20134dd35968116bebf8da11eb17c51a1374
AndersonElit <rodriarri@gmail.com>
2024-11-03 23:12:35 -0500

    base structure

---
+++ infrastructure/entry-points/src/main/java/hexagonal/architecture/infrastructure/entrypoints/controller/TestController.java
@@ -0,0 +1,14 @@
+package hexagonal.architecture.infrastructure.entrypoints.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+
+}
